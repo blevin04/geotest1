5022fbe4d1f2c9c67caaf504992e1a055842edc2
blevin04 <jamestonito04@gmail.com>
2025-04-13 13:13:58 +0300

    added filepicker

--- lib/utils.dart
+++ lib/utils.dart
@@ -1,6 +1,9 @@
 // import 'package:geofence_foreground_service/exports.dart';
+import 'package:file_picker/file_picker.dart';
+import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:location/location.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 final location0 = Location();
 
@@ -10,4 +13,32 @@ Future<List> currentLocation() async {
   currPos.add(locationData.latitude);
   currPos.add(locationData.longitude);
   return currPos;
+}
+
+showsnackbar(BuildContext context, String content) {
+  ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(content)));
+}
+
+Future<String> getContent(BuildContext context, FileType filetype) async {
+  String content = "";
+  Permission.accessMediaLocation.onDeniedCallback(() async {
+    Permission.accessMediaLocation.request();
+    if (await Permission.accessMediaLocation.isDenied) {
+      showsnackbar(context, "Permission denied");
+    }
+    if (await Permission.accessMediaLocation.isGranted) {
+      showsnackbar(context, 'Granted');
+    }
+  });
+
+  FilePickerResult? result = (await FilePicker.platform
+      .pickFiles(type: filetype, allowMultiple: false));
+  if (result != null) {
+    content = result.files.single.path!;
+    // setState(() {});
+  }
+  if (result == null) {
+    showsnackbar(context, 'no file chossen');
+  }
+  return content;
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,11 +5,13 @@
 import FlutterMacOS
 import Foundation
 
+import file_picker
 import flutter_local_notifications
 import location
 import path_provider_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
   FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
   LocationPlugin.register(with: registry.registrar(forPlugin: "LocationPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
